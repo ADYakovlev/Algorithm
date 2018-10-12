c61f7e37f6783df9fa709d9c1e2eda567c7a48a6
ADYakovlev <ay@skb.ru>
2018-10-12 19:59:37 +0500

    simple exp

--- src/main/java/Lesson01.java
+++ src/main/java/Lesson01.java
@@ -3,6 +3,20 @@
  */
 public class Lesson01 {
     public static void main(String[] args) {
-        System.out.println("test");
+        int a = 2; //value
+        int n = 10; //exp
+        long res;
+        res = toExp(a,n);
+        System.out.println(res);
+    }
+
+    public static long toExp(int value, int exp){
+        long res;
+        res = value;
+        for (int i = 1; i < exp; i++) {
+            res = res * value;
+            System.out.println(res);
+        }
+        return res;
     }
 }
